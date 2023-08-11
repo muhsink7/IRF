f34d4642dc193286d30dad400dccc67ae111fead
Abdul Muhsin <muhasink444@gmail.com>
2023-08-11 11:15:24 +0530

    6 menu page implementation (#8)
    * Revert "1 implement getx statemanagement (#5)"
    
    This reverts commit 0305d13c21911a676e6b4ef9a2e7448f373db146.
    
    * 2nd Stage commit
    
    * firebase commit
    
    ---------
    
    Co-authored-by: a2en <ameenahsan619@gmail.com>

--- lib/ui/screens/login_screen/login_controller.dart
+++ lib/ui/screens/login_screen/login_controller.dart
@@ -1,16 +1,16 @@
 import 'package:get/get.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import '../../../router.dart';
 
 class LoginController extends GetxController {
-// RxString to hold the mobile number input
+  // RxString to hold the mobile number input
   RxString mobileNumber = ''.obs;
 
   @override
   void onInit() {
     // TODO: implement onInit
     super.onInit();
-    
   }
 
   // Method to update the mobile number input
@@ -19,6 +19,18 @@ class LoginController extends GetxController {
   }
 
   // Method to handle login button press
+
+  void onLoginButtonPressed() async {
+    try {
+      // Verify the phone number using Firebase Authentication
+      await FirebaseAuth.instance.verifyPhoneNumber(
+        phoneNumber: mobileNumber.value,
+        verificationCompleted: (PhoneAuthCredential credential) {},
+        verificationFailed: (FirebaseAuthException e) {},
+        codeSent: (String verificationId, int? resendToken) {},
+        codeAutoRetrievalTimeout: (String verificationId) {},
+      );
+
   void onLoginButtonPressed() {
     // You can perform any validation or logic related to the mobile number here.
     // For example, you can check if the mobile number is valid before navigating to OTPScreen.
@@ -26,6 +38,11 @@ class LoginController extends GetxController {
       Get.toNamed(RoutePaths.mainScreen);
       print("after navigating otp page");
 
-  }
 
+      // Navigate to the OTP screen
+      Get.toNamed(RoutePaths.mainScreen);
+    } catch (error) {
+      print(error);
+    }
+  }
 }
